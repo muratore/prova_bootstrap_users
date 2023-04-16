d8ebf4bf832e8e309b20c9cc956bfe4bb86ee499
Alessandro Muratore <muratore@gmail.com>
2023-04-16 18:12:26 -0300

    Implementação do filter pelo id

--- assets/script.js
+++ assets/script.js
@@ -1,27 +1,57 @@
-
+const tbody = document.querySelector("tbody");
+const select = document.querySelector("select");
 // Get users
 
-const getAllUsers = async ()=>{
-    const response = await fetch('https://jsonplaceholder.typicode.com/users');
-    const data = await response.json()
-    console.log(data);
-    fillTable(data)
-}
-getAllUsers()
+const getAllUsers = async () => {
+  tbody.innerHTML = "";
+  select.innerHTML = "";
+  const response = await fetch("https://jsonplaceholder.typicode.com/users");
+  const data = await response.json();
+  console.log(data);
+  fillTable(data);
+  createSelection(data);
+};
+getAllUsers();
 
-const fillTable = (users)=>{
-    const tbody = document.querySelector('tbody')
-       
-    users.map((user)=>{
-        tbody.insertAdjacentHTML('beforeend', 
-        `<td scope="col">${user.id}</td>
+const createSelection = (users) => {
+  select.insertAdjacentHTML(
+    "beforeend",
+    ` <option value="null">Escolha um id</option>`
+  );
+  users.map((user) => {
+    select.insertAdjacentHTML(
+      "beforeend",
+      `<option value="${user.id}">${user.id}</option>`
+    );
+  });
+};
+
+const filterById = async (id) => {
+  const response = await fetch(
+    `https://jsonplaceholder.typicode.com/users/?id=${id}`
+  );
+  const data = await response.json();
+  console.log(data);
+  fillTable(data);
+};
+
+const fillTable = (users) => {
+  users.map((user) => {
+    tbody.insertAdjacentHTML(
+      "beforeend",
+      `<td scope="col">${user.id}</td>
         <td>${user.name} </td>
         <td>${user.email} </td>
         <td>${user.address.street}  ${user.address.suite}</td>
         <td>${user.address.city}</td>
         <td>${user.phone}</td>
-        <td>${user.company.name}</td>
-        `      
-        )
-    })
-}
+        <td>${user.company.name}</td>      `
+    );
+  });
+};
+
+const handleFilter = () => {
+  tbody.innerHTML = "";
+  const id = document.querySelector("select").value;
+  filterById(id);
+};
